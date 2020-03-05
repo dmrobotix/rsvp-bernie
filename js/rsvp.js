7a4e795e7bbc1efa0bc391763a439a6b677f57b1
Margot Paez <vansgirl12@me.com>
2020-03-05 11:30:37 -0500

    debugging

--- js/rsvp.js
+++ js/rsvp.js
@@ -31,7 +31,7 @@ formUpload.addEventListener('submit', function(e) {
       phone = data.phone;
       status = data.status;
 
-      document.getElementByClassName('totalRSVP').forEach(function(element,index) {element.innerHTML = fName.length});
+      document.getElementsByClassName('totalRSVP').forEach(function(element,index) {element.innerHTML = fName.length});
       document.getElementById('contactCount').innerHTML = 1;
       document.getElementById('phone').innerHTML = phone[0];
       document.getElementById('messageDisplay').innerHTML = fName[0]+" "+lName[0]
