bd9c1ff703c981b4458070984ce5ff474a952dd7
Margot Paez <vansgirl12@me.com>
2020-03-05 12:04:12 -0500

    adding regex to replace placeholders in message and adding input box for hst name

--- js/rsvp.js
+++ js/rsvp.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
 
 /* form input submit button */
 const formUpload = document.getElementById('formUpload')
+var hName = document.getElementById('hostName').value;
 var message, fName, lName, phone, status;
+var regEx = /\[\w+\]/g
+var regExF = /\[FirstName+\]/g
+var regExL = /\[LastName+\]/g
+var regExH = /\[HostName+\]/g
 formUpload.addEventListener('submit', function(e) {
   e.preventDefault()
   message = document.getElementById('message').value
@@ -31,15 +36,19 @@ formUpload.addEventListener('submit', function(e) {
       phone = data.phone;
       status = data.status;
 
-      //document.getElementsByClassName('totalRSVP').forEach(function(element,index) {element.innerHTML = fName.length});
+      message.replace(regExF,fName[0])
+      message.replace(regExL,lName[0])
+      message.replace(regExH,hName)
+
       const total = document.getElementsByClassName('totalRSVP');
       for(var i=0; i<total.length; i++) {
         total[i].innerHTML = fName.length;
       }
       document.getElementById('contactCount').innerHTML = 1
       document.getElementById('phone').innerHTML = phone[0]
-      document.getElementById('messageDisplay').innerHTML = fName[0]+" "+lName[0]
-      console.log(data)
+      document.getElementById('messageDisplay').innerHTML = message
+      document.getElementById('contactNext').dataset.count += 1
+      //console.log(data)
     }
   })
   .catch(error => {
