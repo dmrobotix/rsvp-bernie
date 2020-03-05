1fe27009f09d043c6e4dccef6bbb9e0dfc486239
Margot Paez <vansgirl12@me.com>
2020-03-05 12:17:28 -0500

    adding code to handle flippng through contacts

--- js/rsvp.js
+++ js/rsvp.js
@@ -7,14 +7,16 @@ $(document).ready(function () {
 /* form input submit button */
 const formUpload = document.getElementById('formUpload')
 var hName = document.getElementById('hostName').value;
-var message, fName, lName, phone, status;
+var message, hName, fName, lName, phone, status;
 var regEx = /\[\w+\]/g
 var regExF = /\[FirstName+\]/g
 var regExL = /\[LastName+\]/g
 var regExH = /\[HostFirstName+\]/g
+
 formUpload.addEventListener('submit', function(e) {
   e.preventDefault()
   message = document.getElementById('message').value
+  hName = document.getElementById('hostName').value
   console.log(message)
   const file = document.getElementById('fileInput').files[0]
   const formData = new FormData()
@@ -40,14 +42,15 @@ formUpload.addEventListener('submit', function(e) {
       message = message.replace(regExL,lName[0])
       message = message.replace(regExH,hName)
 
-      const total = document.getElementsByClassName('totalRSVP');
+      const total = document.getElementsByClassName('totalRSVP')
       for(var i=0; i<total.length; i++) {
         total[i].innerHTML = fName.length;
       }
       document.getElementById('contactCount').innerHTML = 1
       document.getElementById('phone').innerHTML = phone[0]
       document.getElementById('messageDisplay').innerHTML = message
-      document.getElementById('contactNext').dataset.count += 1
+      const counter = Number(document.getElementById('contactNext').dataset.count)
+      document.getElementById('contactNext').dataset.count = counter + 1;
       //console.log(data)
     }
   })
@@ -56,4 +59,22 @@ formUpload.addEventListener('submit', function(e) {
     console.log(error)
   })
 
+})
+
+/* flip through contacts */
+const nextBtn = document.getElementById('contactNext')
+nextBtn.addEventListener('click', function(e) {
+  const counter = Number(document.getElementById('contactNext').dataset.count)
+  if (counter + 1 < phone.length) {
+    message = message.replace(regExF,fName[counter])
+    message = message.replace(regExL,lName[counter])
+    message = message.replace(regExH,hName)
+
+    document.getElementById('contactCount').innerHTML = counter + 1
+    document.getElementById('phone').innerHTML = phone[counter]
+    document.getElementById('messageDisplay').innerHTML = message
+    document.getElementById('contactNext').dataset.count = counter + 1;
+  } else {
+    nextBtn.disabled = true
+  }
 })
